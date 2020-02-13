5f6f52f939232da9ffc85cbab8ebab05dab3645b
ramdanigit <48593749+ramdanigit@users.noreply.github.com>
2020-02-12 22:05:00 -0800

    Add files via upload

---
+++ views/home.js
@@ -0,0 +1,20 @@
+// const axios = require("axios");
+
+// let a = document.getElementById("a");
+// let b = document.getElementById("b");
+// let c = document.getElementById("c");
+// var myTimer;
+// var lightColors;
+// var ServerIP = "192.168.0.14:3500";
+// setInterval(function() {
+//   getData();
+// }, 100);
+
+// function getData() {
+//   var url = "http://" + ServerIP + "/api/parks";
+//   return axios.get(url).then(response => {
+//     // returning the data here allows the caller to get it through another .then(...)
+//     console.log(response.data);
+//     return response.data;
+//   });
+// }
